78e045927e61c9db10c8b23e7198993ec7f6f151
ChrisVoura <crissnchz983@gmail.com>
2025-06-29 01:03:50 -0600

    Update casoPractico.ino

--- casoPractico/casoPractico.ino
+++ casoPractico/casoPractico.ino
@@ -10,57 +10,25 @@
 *Carga de las bibliotecas del sistema      *
 ********************************************/
 
-/********************************************  
+/********************************************
 *Definicion de las constantes del sistema   *
 ********************************************/
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-=======
-=======
->>>>>>> Stashed changes
 #define BTN_ALARMA A0      
 #define BTN_editarAR A1
 #define BTN_SUBIR   A2
 #define BTN_BAJAR A3
 #define BTN_SELECCIONAR A7
->>>>>>> Stashed changes
 
 /*****************************************************
 *Definicion de las variables y objetos del sistema   *
 *****************************************************/
 int SEC_U[4] = {53, 52, 51, 50};
 int SEC_D[4] = {49, 48, 47, 46};
-
 int MIN_U[4] = {45, 44, 43, 42};
 int MIN_D[4] = {41, 40, 39, 38};
-
 int HRS_U[4] = {37, 36, 35, 34};
 int HRS_D[4] = {33, 32, 31, 30};
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-int hours   = 12;
-int minutes = 34;
-int seconds = 50;
-
-/*****************************************************
-*Definicion de funciones y metodos del sistema   *
-*****************************************************/
-void displayBCD(int decena, int pin[4]) {
-  for (int i = 0; i < 4; i++) {
-    digitalWrite(pin[i], (decena >> i) & 1);
-  }
-}
-
-void refrescar() {
-  displayBCD(seconds % 10, SEC_U);
-  displayBCD(seconds / 10, SEC_D);
-  displayBCD(minutes % 10, MIN_U);
-  displayBCD(minutes / 10, MIN_D);
-  displayBCD(hours   % 10, HRS_U);
-  displayBCD(hours   / 10, HRS_D);
-}
-=======
 int hora = 12, minuto = 34, segundo = 50;
 //variables relog
 int alarmaHora = 0, alarmaMinuto = 0;  
@@ -80,27 +48,6 @@ void displayBCD(int digito, int pin[4]) {
 }
 
 void refrescar() {
-=======
-int hora = 12, minuto = 34, segundo = 50;
-//variables relog
-int alarmaHora = 0, alarmaMinuto = 0;  
-bool editar = 0, selMin = 0, parpadeo = 1;
-//variables alarma
-bool modoAlarma = 0;                   
-bool editandoAlarma = 0, selMinAlarma = 0, parpadeoAlarma = 1;
-long t0, tParpadeo, tIndicador;
-
-/*****************************************************
-*Definicion de funciones y metodos del sistema        *
-*****************************************************/
-
-void displayBCD(int digito, int pin[4]) { 
-  for (int i = 0; i < 4; i++) 
-    digitalWrite(pin[i], (digito >> i) & 1); 
-}
-
-void refrescar() {
->>>>>>> Stashed changes
    // Modo alarma
   if (modoAlarma) {
     displayBCD(15, SEC_U);                   
@@ -139,34 +86,8 @@ void refrescar() {
 }
 
 bool presionado(int pin) { return !digitalRead(pin); }
->>>>>>> Stashed changes
 
-//equivalente al BIOS del pc
 void setup() {
-<<<<<<< Updated upstream
-  for (int i = 0; i < 4; i++) {
-    pinMode(SEC_U[i], OUTPUT);
-    pinMode(SEC_D[i], OUTPUT);
-    pinMode(MIN_U[i], OUTPUT);
-    pinMode(MIN_D[i], OUTPUT);
-    pinMode(HRS_U[i], OUTPUT);
-    pinMode(HRS_D[i], OUTPUT);
-  }
-  refrescar();
-} // Final del setup
-
-//Equivalente al SO
-void loop() {
-  delay(250);
-  seconds++;
-  if (seconds == 60) { seconds = 0; minutes++; }
-  if (minutes == 60) { minutes = 0; hours++; }
-  if (hours == 24) hours = 0;
-  refrescar();
-}//Final del loop
-
-
-=======
   int* grupoPines[] = {SEC_U, SEC_D, MIN_U, MIN_D, HRS_U, HRS_D};
   for (int k = 0; k < 6; k++) 
     for (int i = 0; i < 4; i++) 
@@ -177,9 +98,9 @@ void loop() {
   pinMode(BTN_SUBIR, INPUT_PULLUP);
   pinMode(BTN_BAJAR, INPUT_PULLUP);
   pinMode(BTN_SELECCIONAR, INPUT_PULLUP);
-
-   delay(1000);
   
+  delay(1000);
+
   t0 = millis(); 
   tParpadeo = t0;
   tIndicador = t0;
@@ -192,7 +113,7 @@ void loop() {
   static bool presionadoBajar = true;
   static bool presionadoSeleccionar = true;
   static bool presionadoAlarma = true;
-
+  
   // Manejo del botón de alarma
   if (presionado(BTN_ALARMA) && !presionadoAlarma) {
     modoAlarma = !modoAlarma;
@@ -282,8 +203,3 @@ void loop() {
 
   refrescar();
 }
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-
